e8e85bee03fad9480b3e44a7d7c7afad421885cf
Charlie Greene <greeneca@gmail.com>
2014-01-27 23:35:00 -0400

    add yarn validator

--- app/models/yarn.rb
+++ app/models/yarn.rb
@@ -8,6 +8,18 @@ class Yarn
   field :wpi_weight, type: Integer
   field :knit_weight, type: Integer
 
+  #before_validation :calculate_weights
+
+  validate :name_weight_inclusion
+
+  #def calculate_weights
+    
+  #end
+  
+  def name_weight_inclusion
+    errors.add(:name_weight) unless Yarn.name_weights.include?(name_weight)
+  end
+
   def self.name_weights
     ['Thread', 'Cobweb', 'Lace', 'Light Fingering', 'Fingering', 'Sport', 'DK',
      'Worsted', 'Aran', 'Bulky', 'Super Bulky']
