52f63dc8db4ea4808f28d415ba8dfd147dcdd661
Charlie Greene <greeneca@gmail.com>
2014-01-27 23:21:50 -0400

    update form

--- app/models/yarn.rb
+++ app/models/yarn.rb
@@ -7,4 +7,9 @@ class Yarn
   field :ply_weight, type: Integer
   field :wpi_weight, type: Integer
   field :knit_weight, type: Integer
+
+  def self.name_weights
+    ['Thread', 'Cobweb', 'Lace', 'Light Fingering', 'Fingering', 'Sport', 'DK',
+     'Worsted', 'Aran', 'Bulky', 'Super Bulky']
+  end
 end
