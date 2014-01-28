9e0c5436aebdfe997745f801bbb2190bbe0edaa5
Charlie Greene <greeneca@gmail.com>
2014-01-27 21:22:27 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YarnMatch::Application.config.secret_token = '3d1c323f055f91c74d179445bf05ad94b1cc2dc71c7cd72a359ee116c0b68801fa9e0dde859c28695cd487fda65ebc0c2e577d4f77be4f31a2d6d1b38c9774be'
